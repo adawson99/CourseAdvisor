946e9f3f4affb6f39277b600d4d260f0fc186bf4
Audrey <audreystargirl99@gmail.com>
2019-01-14 14:37:00 -0500

    second commit (beginning of database)

--- app/src/main/java/com/example/adawson/courseadvisor/model/Course.java
+++ app/src/main/java/com/example/adawson/courseadvisor/model/Course.java
@@ -1,16 +1,25 @@
 package com.example.adawson.courseadvisor.model;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.HashSet;
 
+@Entity(tableName = "course_table")
 public class Course {
-
+    @PrimaryKey(autoGenerate = true)
+    private int key;
+    @ColumnInfo(name = "id")
     private String id;
-
+    @ColumnInfo(name = "name")
     private String name;
 
    // connects the course to a major
-    private String majorId;
+    @ColumnInfo(name = "major")
+    private String major;
 
+    @ColumnInfo(name = "credits")
     private int credits;
 
    //  applicable latin honors
@@ -19,15 +28,19 @@ public class Course {
     public Course(String id, String name, String majorId, int credits) {
         this.id = id;
         this.name = name;
-        this.majorId = majorId;
+        this.major = majorId;
         this.credits = credits;
     }
+
+    public int getKey() { return key; }
+
+    public void setKey(int key) {this.key = key; }
 
     public String getId() { return id; }
 
     public String getName() { return name; }
 
-    public String getMajor() { return majorId; }
+    public String getMajor() { return major; }
 
     public HashSet<String> getLatinHonors() { return latinHonors; }
 
