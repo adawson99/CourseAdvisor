7a86b95aeaff808c057d29843665aeec6f65c857
Audrey <audreystargirl99@gmail.com>
2019-01-14 19:52:45 -0500

    8th commit - personalizing the semester fragments in Home.java

--- app/src/main/java/com/example/adawson/courseadvisor/Home.java
+++ app/src/main/java/com/example/adawson/courseadvisor/Home.java
@@ -1,16 +1,35 @@
 package com.example.adawson.courseadvisor;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+// changes so that it implements the semester_preview_fragment
 public class Home extends AppCompatActivity {
+
+    private static final String TAG = "logging";
+    // my fragment
+   semester_preview_fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        // initialize the fragment semester titles, this never needs to change or update
+        initializeFragmentTitles(fm);
+
+
+
+
     }
 
     // handler for checkProgress button that triggers information/feedback activity
@@ -20,4 +39,50 @@ public class Home extends AppCompatActivity {
         Intent intent = new Intent(this, CourseSelection.class);
       startActivity(intent);
     }
-}
+
+    public void initializeFragmentTitles(FragmentManager fm) {
+
+        // all the fragments
+        Fragment s1 = fm.findFragmentById(R.id.semester_1_fragment);
+        Fragment s2 = fm.findFragmentById(R.id.semester_2_fragment);
+        Fragment s3 = fm.findFragmentById(R.id.semester_3_fragment);
+        Fragment s4 = fm.findFragmentById(R.id.semester_4_fragment);
+        Fragment s5 = fm.findFragmentById(R.id.semester_5_fragment);
+        Fragment s6 = fm.findFragmentById(R.id.semester_6_fragment);
+        Fragment s7 = fm.findFragmentById(R.id.semester_7_fragment);
+        Fragment s8 = fm.findFragmentById(R.id.semester_8_fragment);
+
+        TextView semester1Title = (TextView) s1.getView().findViewById(R.id.semesterTitle);
+        semester1Title.setText(R.string.s1);
+
+        // semester 2 title
+        TextView semester2Title = (TextView) s2.getView().findViewById(R.id.semesterTitle);
+        semester2Title.setText(R.string.s2);
+
+        // semester 3 title
+        TextView semester3Title = (TextView) s3.getView().findViewById(R.id.semesterTitle);
+        semester3Title.setText(R.string.s3);
+
+        // semester 4 title
+        TextView semester4Title = (TextView) s4.getView().findViewById(R.id.semesterTitle);
+        semester4Title.setText(R.string.s4);
+
+        // semester 5 title
+        TextView semester5Title = (TextView) s5.getView().findViewById(R.id.semesterTitle);
+        semester5Title.setText(R.string.s5);
+
+        // semester 6 title
+        TextView semester6Title = (TextView) s6.getView().findViewById(R.id.semesterTitle);
+        semester6Title.setText(R.string.s6);
+
+        // semester 7 title
+        TextView semester7Title = (TextView) s7.getView().findViewById(R.id.semesterTitle);
+        semester7Title.setText(R.string.s7);
+
+        // semester 8 title
+        TextView semester8Title = (TextView) s8.getView().findViewById(R.id.semesterTitle);
+        semester8Title.setText(R.string.s8);
+    }
+
+    }
+
--- app/src/main/java/com/example/adawson/courseadvisor/semester_preview_fragment.java
+++ app/src/main/java/com/example/adawson/courseadvisor/semester_preview_fragment.java
@@ -1,16 +1,31 @@
 package com.example.adawson.courseadvisor;
 
+//https://guides.codepath.com/android/Creating-and-Using-Fragments
+
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import com.example.adawson.courseadvisor.model.Semester;
 
 
 public class semester_preview_fragment extends Fragment {
 
+    private static final String TAG = "logging";
+
+
+    // each fragmnet is a semester, so we probably want to have this?
+    Semester semester;
 
     public semester_preview_fragment() {
         // Required empty public constructor
@@ -20,9 +35,55 @@ public class semester_preview_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_semester_preview_fragment, container, false);
+        TextView semesterTitle = fragmentView.findViewById(R.id.semesterTitle);
+        semesterTitle.setText("Semester 1");
+        TextView course1 =  fragmentView.findViewById(R.id.course1);
+        course1.setText("Course #1");
+        TextView course2 = fragmentView.findViewById(R.id.course2);
+        course2.setText("Course #2");
+        TextView course3 = fragmentView.findViewById(R.id.course3);
+        course3.setText("Course #3");
+        TextView course4 = fragmentView.findViewById(R.id.course4);
+        course4.setText("Course #4");
+        TextView creditHours = fragmentView.findViewById(R.id.creditHours);
+        creditHours.setText("16");
+
+        // how do we get the different instances?
+        //do we get all the semester ids and then cycle through, and put in the course titles that are oonnected to it
+        // and its credit hours (that get updated when courses are added to it in the joint table)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_semester_preview_fragment, container, false);
+        return fragmentView;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    // triggers after onCreateView()
+    // any view setup should occur here (lookups, attaching view listeners)
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //setup any handles to view objects here
+        // a new adapter is created in on create and this is
+        // set as the view's adapter here
+    }
+
+    // EXPERIMENTATION BELOW
 
 
 }
